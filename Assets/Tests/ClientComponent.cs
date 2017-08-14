843d748324a0f2ea2fcdf97af1aee259b392168b
Endel Dreyer <endel.dreyer@gmail.com>
2017-08-14 16:02:10 +0200

    add initial tests for Client/Room. #21

---
+++ Assets/Tests/ClientComponent.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Colyseus;
+
+public class ClientComponent : MonoBehaviour
+{
+	public Client client;
+	public Room room;
+
+	// Use this for initialization
+	public IEnumerator Start () {
+		client = new Client("ws://localhost:3553");
+
+		yield return StartCoroutine(client.Connect());
+
+		room  = client.Join("chat");
+		room.OnReadyToConnect += (sender, e) => StartCoroutine ( room.Connect() );
+
+		while (true)
+		{
+			client.Recv();
+
+			if (client.error != null)
+			{
+				Debug.LogError ("Error: " + client.error);
+				break;
+			}
+			yield return 0;
+		}
+
+		OnApplicationQuit();
+	}
+
+	void OnDestroy ()
+	{
+		// Make sure client will disconnect from the server
+		room.Leave ();
+		client.Close ();
+	}
+
+	void OnApplicationQuit()
+	{
+		// Ensure the connection with server is closed immediatelly
+		OnDestroy();
+	}
+}
---
+++ Assets/Tests/RoomTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+using Colyseus;
+
+public class RoomTest {
+	ClientComponent component;
+
+	[UnityTest]
+	public IEnumerator TestConnection()
+	{
+		var gameObject = new GameObject();
+		component = gameObject.AddComponent<ClientComponent>();
+
+		yield return new WaitForFixedUpdate();
+
+		component.client.OnOpen += (object sender, System.EventArgs e) => {
+			Assert.NotNull (component.client.id);
+		};
+
+		yield return new WaitForSeconds(0.1f);
+
+		component.room.OnJoin += (object sender, System.EventArgs e) => {
+			Assert.NotNull (component.room.id);
+			Assert.NotNull (component.room.sessionId);
+		};
+
+		Assert.NotNull (component.room.data ["players"]);
+		Assert.NotNull (component.room.data ["messages"]);
+	}
+}
