7e265b10bcffaa3f6d6ad952c99dd4a7a97d0174
Endel Dreyer <endel.dreyer@gmail.com>
2017-08-14 12:00:05 +0200

    add tests for DeltaContainer. #21

---
+++ Assets/Tests/DeltaContainerTest.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+using Colyseus;
+using GameDevWare.Serialization;
+
+public class DeltaContainerTest {
+	DeltaContainer container;
+
+	[SetUp]
+	public void Init () {
+		container = new DeltaContainer (GetRawData());
+	}
+
+	[TearDown]
+	public void Dispose () {
+		container.RemoveAllListeners ();
+	}
+
+	[Test]
+	public void ListenAddRemove() {
+		var newData = GetRawData ();
+
+		var players = (IndexedDictionary<string, object>) newData ["players"];
+		players.Remove ("key1");
+		players.Add ("key3", new { value = "new" });
+
+		var listenCalls = 0;
+		container.Listen ("players/:id", (DataChange change) => {
+			listenCalls++;
+
+			if (change.operation == "add") {
+				Assert.AreEqual("key3", change.path["id"]);
+				Assert.AreEqual(new { value = "new" }, change.value);
+
+			} else if (change.operation == "remove") {
+				Assert.AreEqual("key1", change.path["id"]);
+			}
+		});
+
+		container.Set (new IndexedDictionary<string, object>(newData));
+		Assert.AreEqual (2, listenCalls);
+	}
+
+	[Test]
+	public void ListenReplace() {
+		var newData = GetRawData ();
+
+		var players = (IndexedDictionary<string, object>) newData ["players"];
+		players ["key1"] = new IndexedDictionary<string, object>(new Dictionary<string, object> {
+			{"id", "key1"},
+			{"position", new IndexedDictionary<string, object>(new Dictionary<string, object> {
+				{"x", 50},
+				{"y", 100}
+			})}
+		});
+		newData ["players"] = players;
+
+		var listenCalls = 0;
+		container.Listen ("players/:id/position/:axis", (DataChange change) => {
+			listenCalls++;
+
+			Assert.AreEqual(change.path["id"], "key1");
+
+			if (change.path["axis"] == "x") {
+				Assert.AreEqual(change.value, 50);
+
+			} else if (change.path["axis"] == "y") {
+				Assert.AreEqual(change.value, 100);
+			}
+		});
+
+		container.Set (new IndexedDictionary<string, object>(newData));
+		Assert.AreEqual (2, listenCalls);
+	}
+
+	protected IndexedDictionary<string, object> GetRawData () {
+		var data = new IndexedDictionary<string, object> ();
+		var players = new IndexedDictionary<string, object> ();
+
+		players.Add("key1", new IndexedDictionary<string, object>(new Dictionary<string, object> { 
+			{"id", "key1"},
+			{"position", new IndexedDictionary<string, object>(new Dictionary<string, object> { 
+				{"x", 0},
+				{"y", 10}
+			})}
+		}));
+		players.Add("key2", new IndexedDictionary<string, object>(new Dictionary<string, object> { 
+			{"id", "key2"},
+			{"position", new IndexedDictionary<string, object>(new Dictionary<string, object>{ 
+				{"x", 10}, 
+				{"y", 20}
+			})}
+		}));
+
+		data.Add ("players", players);
+		data.Add ("messages", new string[]{ "one", "two", "three" });
+		return data;
+	}
+
+}
