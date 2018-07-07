c0967f0a27836bf13627e5277a967457ad77a67f
Bhavik Charola <bhavikcharola@gmail.com>
2018-07-07 11:08:11 +0530

    Fix GetAvailableRooms method

---
+++ Assets/Plugins/Colyseus/Utils/ObjectExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Colyseus
+{
+    public static class ObjectExtensions
+    {
+        public static T ToObject<T>(object source) where T : class, new()
+        {
+            var someObject = new T();
+            var someObjectType = someObject.GetType();
+
+            foreach (var item in (IDictionary<string, object>)source) {
+                someObjectType
+                         .GetProperty(item.Key)
+                         .SetValue(someObject, item.Value, null);
+            }
+
+            return someObject;
+        }
+    }
+}
