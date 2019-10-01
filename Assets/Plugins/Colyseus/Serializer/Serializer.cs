f4df0bec794f8570215ceb8ec49506fa77a09c09
Daniel Hix <danieljacobhix@gmail.com>
2019-10-01 11:25:12 -0500

    Adding new exception

--- Assets/Plugins/Colyseus/Serializer/Serializer.cs
+++ Assets/Plugins/Colyseus/Serializer/Serializer.cs
@@ -13,4 +13,14 @@ namespace Colyseus
 	    void Teardown ();
     	void Handshake (byte[] bytes, int offset);
 	}
+
+	public class NotSerializableException : Exception
+	{
+	  public MyException() { }
+	  public MyException( string message ) : base( message ) { }
+	  public MyException( string message, Exception inner ) : base( message, inner ) { }
+	  protected MyException( 
+		System.Runtime.Serialization.SerializationInfo info, 
+		System.Runtime.Serialization.StreamingContext context ) : base( info, context ) { }
+	}
 }
