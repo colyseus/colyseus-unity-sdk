944dbd802a3418d7618a477ddcb1f8e1d427cb6b
mitchLucid <35048848+mitchLucid@users.noreply.github.com>
2021-05-28 15:48:55 -0700

    Missed "using"

--- Assets/Colyseus/Runtime/Scripts/Models/ColyseusConnection.cs
+++ Assets/Colyseus/Runtime/Scripts/Models/ColyseusConnection.cs
@@ -1,8 +1,8 @@
+using UnityEngine;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using NativeWebSocket;
-
 // ReSharper disable InconsistentNaming
 
 namespace Colyseus
