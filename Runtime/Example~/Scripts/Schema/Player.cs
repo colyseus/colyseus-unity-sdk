9dc7b0bf4cb2b25326002acd2add4dd95c4f1d7a
endel <endel.dreyer@gmail.com>
2022-08-31 13:00:33 +0000

    Update upm to output generated at 348bc56

---
+++ Runtime/Example~/Scripts/Schema/Player.cs
@@ -0,0 +1,18 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 1.0.38
+// 
+
+using Colyseus.Schema;
+
+public partial class Player : Schema
+{
+	[Type(0, "number")]
+	public float x = default(float);
+
+	[Type(1, "number")]
+	public float y = default(float);
+}
+
--- Runtime/Example~/Scripts/Utils/DontDestroyTag.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class DontDestroyTag : MonoBehaviour
-{
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-}
