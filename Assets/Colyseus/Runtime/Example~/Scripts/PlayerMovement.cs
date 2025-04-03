7a8b62a22e80a11c29d7417ae22dd149d2bc3ffb
Joe Meineke <joe.meineke@gmail.com>
2025-04-02 21:41:45 -0400

    Modify MyRoom.ts to delete player on leave and to set state according to the online example, add dictionary key check to Callbacks.cs to prevent a KeyNotFound exception, update PlayerMovement.cs to use v.16. Sprite was missing on player, so I just assigned an existing sprite to it. modified .gitignore to exclude IntelliJ artifacts. (#241)
    Co-authored-by: Joseph C Meineke <jmeineke@aep.com>

--- Assets/Colyseus/Runtime/Example~/Scripts/PlayerMovement.cs
+++ Assets/Colyseus/Runtime/Example~/Scripts/PlayerMovement.cs
@@ -1,56 +1,79 @@
 using System;
+using System.Threading.Tasks;
+using Colyseus;
+using Colyseus.Schema;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10f;
-    private bool _moving;
-    private NetworkManager _networkManager;
-    private Vector2 _targetPosition;
+	public float speed = 10f;
+	private bool _moving;
+	private NetworkManager _networkManager;
+	private Vector2 _targetPosition;
+	private StateCallbackStrategy<MyRoomState> _callBacks;
 
-    public async void Start()
-    {
-        // Initialize game room.
-        _networkManager = gameObject.AddComponent<NetworkManager>();
-        await _networkManager.JoinOrCreateGame();
-        
-        // Assigning listener for incoming messages
-        _networkManager.GameRoom.OnMessage<string>("welcomeMessage", message =>
-        {
-            Debug.Log(message);
-        });
-        
-        // Set player's new position after synchronized the mouse click's position with the Colyseus server.
-        
-        _networkManager.GameRoom.State.OnChange(() =>
-        {
-            var player = _networkManager.GameRoom.State.players[_networkManager.GameRoom.SessionId];
-            _targetPosition = new Vector2(player.x, player.y);
-            _moving = true;
-        });
+	private void Awake()
+	{
+		_networkManager = gameObject.AddComponent<NetworkManager>();
+	}
 
-        _networkManager.GameRoom.State.players.OnAdd((key, player) =>
-        {
-            Debug.Log($"Player {key} has joined the Game!");
-        });
-    }
+	private async void Start()
+	{
+		await _networkManager.JoinOrCreateGame();
+		registerListeners();
+	}
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            // Synchronize mouse click position with the Colyseus server.
-            _networkManager.PlayerPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        }
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Synchronize mouse click position with the Colyseus server.
+			_networkManager.PlayerPosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+		}
 
-        if (_moving && (Vector2)transform.position != _targetPosition)
-        {
-            var step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, _targetPosition, step);
-        }
-        else
-        {
-            _moving = false;
-        }
-    }
+		if (_moving && (Vector2)transform.position != _targetPosition)
+		{
+			var step = speed * Time.deltaTime;
+			transform.position = Vector2.MoveTowards(transform.position, _targetPosition, step);
+		}
+		else
+		{
+			_moving = false;
+		}
+	}
+
+	private void registerListeners()
+	{
+		// Retrieve the StateCallbackStrategy object
+		_callBacks = Callbacks.Get(_networkManager.GameRoom);
+
+		// Assign listener for incoming messages
+		_networkManager.GameRoom.OnMessage<string>("welcomeMessage", message =>
+		{
+			Debug.Log(message);
+		});
+
+		// Assign listeners for state change/add/remove
+		_networkManager.GameRoom.OnStateChange += (changedState, firstState) =>
+		{
+			if (!firstState)
+				return;
+
+			_callBacks.OnAdd(addedState => addedState.players, (sessionId, addedPlayer) =>
+			{
+				Debug.Log($"player {sessionId} joined)");
+
+				_callBacks.OnChange(addedPlayer, () =>
+				{
+					_targetPosition = new Vector2(addedPlayer.x, addedPlayer.y);
+					_moving = true;
+				});
+			});
+
+			_callBacks.OnRemove(removedState => removedState.players, (sessionId, removedPlayer) =>
+			{
+				Debug.Log($"player {sessionId} left");
+			});
+		};
+	}
 }
